015b62f40e34d003cc63626ee783e80b4a6e739d
root <root@dywu-linux.homeoffice.wal-mart.com>
2014-01-19 22:30:34 -0800

    Determine whether an integer is a palindrome. Do this without extra space.

---
+++ isPalindrome.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+use Data::Dumper;
+
+#Determine whether an integer is a palindrome. Do this without extra space.
+
+my ($par)  = @ARGV;
+my $result = isPalindrome($par);
+$result? print "Yes\n" : print "No\n";
+
+my $result2 = isPalindrome2($par);
+$result2? print "Yes\n" : print "No\n";
+
+
+sub isPalindrome(){
+    #just like reverse integer
+    my ($num) = @_;
+    my $org = $num;
+    my $res   = 0;
+    while ($num > 0 ){
+        $res = $res*10 + $num%10;
+        $num = int ($num/10);   
+    }
+    return ($org == $res); 
+}
+
+sub isPalindrome2(){
+    my ($num) = @_;
+    my @str = split //,$num;
+    my $res = 1;
+    for (my $i=0;$i<($#str+1)/2;$i++){
+        $res = 0 && last unless (@str[$i] == @str[$#str-$i] );
+    } 
+    return $res;
+}
