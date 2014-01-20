9cc8f8cb67886b706b8b8e65a0bdc8252a65adb2
root <root@dywu-linux.homeoffice.wal-mart.com>
2014-01-19 21:04:02 -0800

    fix issue

--- longSubstr.pl
+++ longSubstr.pl
@@ -12,32 +12,18 @@ print Dumper $result;
 sub lengthOfLongestSubstring(){
     my ($str) = @_;
     my @str = split //, $str;
-    my ($wHash,$result);
+    my $tmpLen;
     my $len = $#str;     
     for (my $i=0 ; $i<=$len ; $i++){
         my $tmpStr;
         foreach (@str){
-            $tmpStr .= $_;
-            $wHash->{$tmpStr} += 1;
+            last if ($tmpStr =~ $_);
+            $tmpStr .= $_ ;
+            my @tmpLenArr = split //,$tmpStr;
+            $tmpLen    = $#tmpLenArr if ($#tmpLenArr >= $tmpLen );
         }
         shift @str;
     }
-    my $resStr;
-    my $resLen = 0;
-    my $resRep = 0;
-    while (my ($key,$value) = each %$wHash){
-        my @tmpS = split //,$key;
-        if ($value >= $resRep ){
-            $resLen = 0 if ($value > $resRep);
-            if ($#tmpS >= $resLen){
-                $resStr = $key;
-                $resLen = $#tmpS;
-                $resRep = $value;
-            }
-        }
-    } 
-    $result->{'str'} = $resStr;
-    $result->{'len'} = $resLen+1;
 
-    return $result;
+    return $tmpLen+1;
 }
